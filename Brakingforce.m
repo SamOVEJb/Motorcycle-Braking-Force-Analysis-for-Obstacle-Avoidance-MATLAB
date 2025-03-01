5e62b2caf8af5a18d600c2f26afec06875507de4
SAMAH JABRI <samahjabri5@gmail.com>
2025-03-01 05:07:27 +0100

    MATLAB Code

---
+++ Brakingforce.m
@@ -0,0 +1,18 @@
+clear all
+close all
+clc
+% Breaking Force required to stop a vehicle before it hits Dog
+%Inputs
+m1=180; % mass of the motorcycle in kg
+m2=75; % mass of the rider in kg
+m=m1+m2; % Total mass
+d=200; % distance from motorcycle to dog 
+v=linspace(10,50,100); % velocity of vehicle ranging from 10 m/s to 50 m/s
+
+%% Breaking force
+F=(m*v.^2)/(2*d);
+plot(v,F,'r','linewidth',3)
+xlabel("Velocity(m)")
+ylabel("BreakingForce(N)")
+title("Breaking force calculations")
+
