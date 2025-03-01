baee2cd93a4cfe0ec9178c538865a924969392bd
SAMAH JABRI <samahjabri5@gmail.com>
2025-03-01 05:08:18 +0100

    Brakingforce.m

--- Brakingforce.m
+++ Brakingforce.m
@@ -1,7 +1,7 @@
 clear all
 close all
 clc
-% Breaking Force required to stop a vehicle before it hits Dog
+% Braking Force required to stop a vehicle before it hits Dog
 %Inputs
 m1=180; % mass of the motorcycle in kg
 m2=75; % mass of the rider in kg
@@ -9,10 +9,10 @@ m=m1+m2; % Total mass
 d=200; % distance from motorcycle to dog 
 v=linspace(10,50,100); % velocity of vehicle ranging from 10 m/s to 50 m/s
 
-%% Breaking force
+%% Braking force
 F=(m*v.^2)/(2*d);
 plot(v,F,'r','linewidth',3)
 xlabel("Velocity(m)")
-ylabel("BreakingForce(N)")
-title("Breaking force calculations")
+ylabel("BrakingForce(N)")
+title("Braking force calculations")
 
